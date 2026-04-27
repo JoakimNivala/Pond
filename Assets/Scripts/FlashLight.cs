858a6da024c877793ed979b89ba7e88e3ec001e3
JoakimNivala <100118821+JoakimNivala@users.noreply.github.com>
2026-04-27 16:14:05 +0300

    Early changes to minimap

--- Assets/Scripts/FlashLight.cs
+++ Assets/Scripts/FlashLight.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -7,6 +8,9 @@ public class FlashLight : MonoBehaviour
     public bool isOn = true;
     public float horizontalSpeed = 2.0F;
     public float verticalSpeed = -2.0F;
+
+    [SerializeField]
+    private GameObject Camera;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -20,6 +24,7 @@ public class FlashLight : MonoBehaviour
         {
             isOn = !isOn;
             _FlashLight.SetActive(isOn);
+            _FlashLight.transform.rotation = Camera.transform.rotation;
         }
         if (Input.GetKey(KeyCode.LeftControl))
         {
--- Assets/Scripts/MiniMapCamera.cs
+++ Assets/Scripts/MiniMapCamera.cs
@@ -2,25 +2,42 @@ using UnityEngine;
 
 public class MiniMapCamera : MonoBehaviour
 {
-    public Shader replacementShader;
-    public Camera MiniMapCam;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-  
-    void OnValidate()
-    {
-        
-        GetComponent<Camera>().SetReplacementShader(replacementShader, "RenderType");
-        
+    public RectTransform marker; //player pointer image
+    public RectTransform mapImage;//Map screenshot used in canvas
+    public Transform playerReference;//player
+    public Transform[] mapEdges;//4 edges, make sure its in rectangle.
+    public Vector2 offset;//Adjust the value to match you map
 
-    }
+    private Vector2 mapDimentions;
+    private Vector2 areaDimentions;
 
-    void OnDisable()
+    private void Start()
     {
-        GetComponent<Camera>().ResetReplacementShader();
+        for (int i = 0; i < mapEdges.Length; i++)
+            for (int j = i + 1; j < mapEdges.Length; j++)
+            {
+                if (mapEdges[j].position.x < mapEdges[i].position.x || mapEdges[j].position.z < mapEdges[i].position.z)
+                {
+                    Transform temp = mapEdges[j];
+                    mapEdges[j] = mapEdges[i];
+                    mapEdges[i] = temp;
+                }
+            }
+        mapDimentions = new Vector2(mapImage.sizeDelta.x, mapImage.sizeDelta.y);
+        areaDimentions.x = mapEdges[1].position.x - mapEdges[0].position.x;
+        areaDimentions.y = mapEdges[2].position.z - mapEdges[0].position.z;
     }
 
     private void Update()
     {
-       MiniMapCam.orthographicSize += Input.mouseScrollDelta.y * 5.5f;
+        SetMarketPosition();
+    }
+
+    private void SetMarketPosition()
+    {
+        Vector3 distance = playerReference.position - mapEdges[0].position;
+        Vector2 coordinates = new Vector2(distance.x / areaDimentions.x, distance.z / areaDimentions.y);
+        marker.anchoredPosition = new Vector2(coordinates.x * mapDimentions.x, coordinates.y * mapDimentions.y) + offset;
+        marker.rotation = Quaternion.Euler(new Vector3(0, 0, -playerReference.eulerAngles.y));
     }
 }
