977cc75eae04463289fd0ca27483879c1733ef26
JoakimNivala <100118821+JoakimNivala@users.noreply.github.com>
2026-04-27 14:48:09 +0300

    changing to urp

--- Assets/Scripts/MiniMapCamera.cs
+++ Assets/Scripts/MiniMapCamera.cs
@@ -2,16 +2,15 @@ using UnityEngine;
 
 public class MiniMapCamera : MonoBehaviour
 {
-    public Shader replacementShaderTxtr;
-    public Shader replacementShaderClr;
+    public Shader replacementShader;
     public Camera MiniMapCam;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
   
     void OnValidate()
     {
         
-        GetComponent<Camera>().SetReplacementShader(replacementShaderTxtr, "RenderType");
-        GetComponent<Camera>().SetReplacementShader(replacementShaderClr, "RenderType");
+        GetComponent<Camera>().SetReplacementShader(replacementShader, "RenderType");
+        
 
     }
 
